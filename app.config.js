7d3a2303ee17f200acc3a3f6476eba31983863f0
Tam Nguyen <t.nguyen976@mybvc.ca>
2025-12-10 22:14:44 -0700

    chore: link EAS projectId; set appVersionSource

--- app.config.js
+++ app.config.js
@@ -1,8 +1,16 @@
+// app.config.js
 module.exports = ({ config }) => ({
   ...config,
   name: 'VolunTeam',
   slug: 'volunteam',
   version: '1.0.0',
+  extra: {
+    ...(config.extra || {}),
+    eas: {
+      ...(config.extra?.eas || {}),
+      projectId: '021f174a-33c6-440c-908f-49a4d7d3d912', // <- from EAS prompt
+    },
+  },
   ios: { ...(config.ios || {}), bundleIdentifier: 'com.tamnguyen976.volunteam' },
   android: { ...(config.android || {}), package: 'com.tamnguyen976.volunteam' },
 });
